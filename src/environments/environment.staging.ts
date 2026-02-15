70d9bbb316bf111a6759e94a7b3788d791af4ebd
Wesley Carvalho <crafael.wesley@gmail.com>
2026-02-15 10:44:40 -0300

    att

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
 
-  apiUrl: 'https://api.guildsystem.com.br/',
+  apiUrl: 'https://api.guildsystem.com.br',
 
-  wsUrl: 'https://api.guildsystem.com.br/',
+  wsUrl: 'https://api.guildsystem.com.br',
 };
