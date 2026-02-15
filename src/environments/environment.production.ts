3eaaa1a392991c335ce9dbcffc5013229b391039
Wesley Carvalho <crafael.wesley@gmail.com>
2026-02-15 10:42:33 -0300

    api url

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:3000',
-  wsUrl: 'http://localhost:3000',
+  apiUrl: 'http://localhost:3001',
+  wsUrl: 'http://localhost:3001',
 };
