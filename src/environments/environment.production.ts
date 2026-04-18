38c1505ddc3f26e1088db8a2e5e236f27b503ea1
wesleycarvalho-eng <crafael.wesley@gmail.com>
2026-04-18 15:40:20 -0300

    env

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:3000',
-  wsUrl: 'http://localhost:3000',
+  apiUrl: 'https://api.guildsystem.com.br',
+  wsUrl: 'https://api.guildsystem.com.br',
 };
